10591f83db74f2c7f849e6100e6a5303060d9d43
pawelokunski <pawel.okunski@wp.pl>
2025-07-14 12:45:41 +0200

    fix: raise AlrearyReturned error properly

--- app/services/return_device_from_user.rb
+++ app/services/return_device_from_user.rb
@@ -11,8 +11,8 @@ class ReturnDeviceFromUser
     raise RegistrationError::Unauthorized unless @user.id == @from_user_id
 
     device = Device.find_by!(serial_number: @serial_number)
-    assignment = device.device_assignments.active.find_by!(user_id: @user.id)
-
+    assignment = device.device_assignments.active.find_by(user_id: @user.id)
+    raise ReturningError::AlreadyReturned unless assignment
     DeviceAssignment.transaction do
       assignment.update!(returned_at: Time.current)
       device.update!(user: nil)
